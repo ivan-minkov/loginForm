a33fe04250006b639a150968606b3fdaa24a3fac
Ivan Minkov <i.minkov@slicecrowd.com>
2015-02-06 09:55:42 +0200

    Test upload

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,23 +1,34 @@
 'use strict';
+/*jshint sub:true*/
 
-describe('Controller: mainCtrl', function () {
+describe('with httpBackend', function() {
 
-  // load the controller's module
-  beforeEach(module('loginApp'));
+beforeEach(module('loginApp'));
 
-  var MainCtrl,
-    scope;
+ var loginCtrl, scope;
 
-  // Initialize the controller and a mock scope
+ // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('mainCtrl', {
+      scope = $rootScope.$new();
+      loginCtrl = $controller('loginCtrl', {
       $scope: scope
     });
+
+    // $httpBackend.when('GET', 'https://api.github.com/users/jasonmore').respond({things: 'and stuff'});
+    // $httpBackend.flush();
   }));
 
-  it('should be the first test', function () {
+it('should get login success',
+  inject(function(LoginService, $httpBackend) {
 
-  });
+    $httpBackend.expect('POST', 'https://api.mydomain.com/login').respond({ success : true });
 
+    LoginService.login('test@test.com', 'password')
+      .then(function(data) {
+        expect(data.success).toBeTruthy();
+        // expect(1).toBe(1);
+    });
+
+  $httpBackend.flush();
+}));
 });
